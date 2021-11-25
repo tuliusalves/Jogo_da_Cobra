c6968885d56429892b1b2ec83d9e6632e3ef469b
tuliusalves <tulius_sr@outlook.com>
2021-11-25 15:08:59 -0300

    Adicionando movimento da cobra

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ snake[0]={
     x:8* box,
     y:8*box
 }
-
+let direction= "right";//Variável da direção do movimento da cobra.
 function criarBG(){
     context.fillStyle = "lightgreen";
     context.fillRect(0,0,16 *box, 16*box);
@@ -17,5 +17,28 @@ function criarCobrinha(){
         context.fillRect(snake[i].x,snake[i].y,box,box);
     }
 }
-criarBG();
-criarCobrinha();
+//Função de intervalo de tempo
+function iniciarJogo(){
+    criarBG();
+    criarCobrinha();
+    //ponto de partida baseado nas posições x e y
+    let snakeX= snake[0].x;
+    let snakeY=snake[0].y;
+
+    //cordenadas
+    if(direction =="right") snakeX +=box;
+    if(direction =="left") snkaeX -=box;
+    if(direction == "up") snakeY -=box;
+    if(direction == "down") snakeY +=box;
+
+    //Retirando o último elemento do array
+    snake.pop();
+
+    //Acrescentando uma nova cabeça
+    let newHead ={
+        x: snakeX,
+        y: snakeY
+    }
+    snake.unshift(newHead);
+}
+let jogo = setInterval(iniciarJogo,100);// Variável de intervalo de tempo de início de jogo
