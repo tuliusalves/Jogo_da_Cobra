9a254fd7fdfc72177398b885210f7557e541e8d3
tuliusalves <tulius_sr@outlook.com>
2021-11-25 22:36:37 -0300

    Fazendo o game over

--- scripts.js
+++ scripts.js
@@ -38,13 +38,21 @@ function update(event){
 }
 //Função de intervalo de tempo
 function iniciarJogo(){
+    
     //realizando retorno ao passar uma parede
     if(snake[0].x>15 * box && direction =="right")snake[0].x=0;
     if(snake[0].x<0 && direction =="left")snake[0].x=16*box;
     if(snake[0].y>15 *box && direction =="down")snake[0].y=0;
     if(snake[0].y<0 && direction =="up")snake[0].y=16*box;
 
-   
+   /*loop for checando se cada cordenada se choca com i, ao fazer
+    isso o jogo será finalizado*/
+    for( i=1;i<snake.length;i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert("Game over :(");
+        }
+    }
     
     criarBG();
     criarCobrinha();
